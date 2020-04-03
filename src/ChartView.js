f61c68960efa25849417b09647c7ffd373f2936f
romainpajda <48944361+romainpajda@users.noreply.github.com>
2020-04-03 17:24:49 +0200

    Ajout des dates pour remplacer les undefined sur le graphe

--- src/ChartView.js
+++ src/ChartView.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 import { dummyModel } from './model/sir';
+import { generer_dates } from './model/generateur_dates'
+
+const jour_0 = new Date(2020, 0, 23);
+
+var tab_dates = generer_dates(jour_0, dummyModel().saints.length) ;
 
 export const Chart = ({ s0, lambda, beta }) => {
     const { saints, infectes, retires } = dummyModel(s0, lambda, beta);
     const lineData = {
-        labels: new Array(saints.length),
+        labels: tab_dates,
         datasets: [
             {
                 label: ['Population saine'],
