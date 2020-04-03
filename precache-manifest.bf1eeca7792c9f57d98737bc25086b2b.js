5f4d755ad8ab974fa7eb0e346e44dc301b661cd9
pem <pierre-etienne.moreau@loria.fr>
2020-04-03 15:23:30 +0200

    Updates

---
+++ precache-manifest.bf1eeca7792c9f57d98737bc25086b2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "918d8d03fa0605742b3ae20e2bd875b9",
+    "url": "/commando-covid/index.html"
+  },
+  {
+    "revision": "aa62b0df870ea3be721e",
+    "url": "/commando-covid/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c6bebb17fec89edcc293",
+    "url": "/commando-covid/static/js/2.cfdd441a.chunk.js"
+  },
+  {
+    "revision": "b82dc1caedd6da17de2528470bfc0c08",
+    "url": "/commando-covid/static/js/2.cfdd441a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa62b0df870ea3be721e",
+    "url": "/commando-covid/static/js/main.12c7222a.chunk.js"
+  },
+  {
+    "revision": "cde06f8daaabe74b8b6c",
+    "url": "/commando-covid/static/js/runtime-main.593b9fdf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7751209411c5f3783236e05b5e6759ef.js"
+  "/commando-covid/precache-manifest.bf1eeca7792c9f57d98737bc25086b2b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/commando-covid/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
