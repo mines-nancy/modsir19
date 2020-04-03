39f597b78358989d13c62bcd810ba1cf54002cec
Paul54460 <paul.festor@gmail.com>
2020-04-03 10:02:46 +0200

    Changed clas -> const and used React.userState

--- src/ChartView.js
+++ src/ChartView.js
@@ -2,48 +2,44 @@ import React from 'react';
 import { Line } from 'react-chartjs-2';
 import { dummyModel } from './model/sir';
 
-class Chart extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            lineData: {
-                labels: new Array(dummyModel().saints.length),
-                datasets: [
-                    {
-                        label: ['Population saine'],
-                        data: dummyModel().saints,
-                    },
-                    {
-                        label: ['Population infectée'],
-                        data: dummyModel().infectes,
-                    },
-                    {
-                        label: ['Population rétablie'],
-                        data: dummyModel().retires,
-                    },
-                ],
-            },
-        };
-    }
+const Chart = () => {
+    const initialState = {
+        lineData: {
+            labels: new Array(dummyModel().saints.length),
+            datasets: [
+                {
+                    label: ['Population saine'],
+                    data: dummyModel().saints,
+                },
+                {
+                    label: ['Population infectée'],
+                    data: dummyModel().infectes,
+                },
+                {
+                    label: ['Population rétablie'],
+                    data: dummyModel().retires,
+                },
+            ],
+        },
+    };
+    const [state, setState] = React.useState(initialState);
 
-    render() {
-        return (
-            <div className="Chart">
-                <Line
-                    data={this.state.lineData}
-                    width="10"
-                    height="10"
-                    options={{
-                        title: {
-                            display: true,
-                            text: 'Un graphique',
-                            fontSize: 25,
-                        },
-                    }}
-                />
-            </div>
-        );
-    }
-}
+    return (
+        <div className="Chart">
+            <Line
+                data={state.lineData}
+                width="10"
+                height="10"
+                options={{
+                    title: {
+                        display: true,
+                        text: 'Un graphique',
+                        fontSize: 25,
+                    },
+                }}
+            />
+        </div>
+    );
+};
 
 export default Chart;
