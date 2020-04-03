f6828b24a56eda5bc6aa499b064d35b780d69ada
pem <pierre-etienne.moreau@loria.fr>
2020-04-03 11:37:34 +0200

    Updates

---
+++ precache-manifest.0c040db51a6d05d0908d5acb9f838f62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "463682b2c4f455553a355f3c9e115e06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "706f61780ee96a65a5e6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "be8b848edbc6fd79d51f",
+    "url": "/static/js/2.bb98ed1f.chunk.js"
+  },
+  {
+    "revision": "b82dc1caedd6da17de2528470bfc0c08",
+    "url": "/static/js/2.bb98ed1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "706f61780ee96a65a5e6",
+    "url": "/static/js/main.396ba1e6.chunk.js"
+  },
+  {
+    "revision": "ac2e1387c3469485d72d",
+    "url": "/static/js/runtime-main.b74b5e04.js"
+  }
+]);
