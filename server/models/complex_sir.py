e334857e78044a1ecb512be21514e8f858d8dd81
Paul54460 <paul.festor@gmail.com>
2020-04-05 22:45:35 +0200

    Suite de descriptions

--- server/models/complex_sir.py
+++ server/models/complex_sir.py
@@ -73,32 +73,32 @@ def model_day(recovered: [float], exposed: [float], infected: [float], hospitali
               Tei: int, Tir: int, Tih: int, Thr: int, Thic: int, Tice: int,
               time: int) -> {str: [float]}:
     """
-    Models a day od simulation
-    :param recovered:
-    :param exposed:
-    :param infected:
-    :param hospitalized:
-    :param intensive_care:
+    Models a day of simulation
+    :param recovered: Time series of the number of people who recovered so far (one day time step)
+    :param exposed: Time series of the number of people who are exposed each day (one day time step)
+    :param infected: Time series of the number of people who were infected each day so far (one day time step)
+    :param hospitalized: Time series of the number of people who were hospitalized each day so far (one day time step)
+    :param intensive_care: Time series of the number of people who were hospitalized each day so far (one day time step)
     :param exit_intensive:
     :param dead:
     :param new_infected:
     :param new_hospitalized:
     :param new_intensive_care:
     :param new_exit_intensive:
-    :param Kei:
-    :param Kir:
-    :param Kih:
-    :param Khic:
-    :param Khr:
-    :param Ked:
-    :param Ker:
-    :param Tei:
-    :param Tir:
-    :param Tih:
-    :param Thr:
-    :param Thic:
-    :param Tice:
-    :param time:
+    :param Kei: Coefficient from exposed to infected
+    :param Kir: Coefficient from infected to recovered
+    :param Kih: Coefficient from infected to hospitalized
+    :param Khic: Coefficient from hospitalized to intensive care
+    :param Khr: Coefficient from hospitalized to recovered
+    :param Ked: Coefficient from exit intensive to dead
+    :param Ker: Coefficient from exit intensive to recovered
+    :param Tei: Delay between being infected and infecting others
+    :param Tir: Delay between being infected and recovering
+    :param Tih: Delay between being infected and being sent to gospital
+    :param Thr: Delay between being sent to gospital and recovering
+    :param Thic: Delay between being sent to gospital and then sent to intensive care
+    :param Tice: Delay between entering and exiting intensive care services
+    :param time: Day after start day the simulation is currently at
     :return:
     """
     # On copie les listes pour ne pas avoir de problème
@@ -141,9 +141,9 @@ def model_day(recovered: [float], exposed: [float], infected: [float], hospitali
 def expose_day(time: int, Kei: float, Tei: int, exposed: [float], infected: [float]) -> float:
     """
     Computes the volume of people who are exposed at day time
-    :param time:
-    :param Kei:
-    :param Tei:
+    :param time: Day after start day the simulation is currently at
+    :param Kei: Coefficient from exposed to infected
+    :param Tei: Delay between being infected and infecting others
     :param exposed:
     :param infected:
     :return: Volume of the exposed population
@@ -161,13 +161,13 @@ def infect_day(time: int, Kei: float, Kir: float, Kih: float,
                exposed: [float], infected: [float], new_infected: [float]) -> float:
     """
     Computes the volume of people who are infected at day time
-    :param time:
-    :param Kei:
-    :param Kir:
-    :param Kih:
-    :param Tei:
-    :param Tir:
-    :param Tih:
+    :param time: Day after start day the simulation is currently at
+    :param Kei: Coefficient from exposed to infected
+    :param Kir: Coefficient from infected to recovered
+    :param Kih: Coefficient from infected to hospitalized
+    :param Tei: Delay between being infected and infecting others
+    :param Tir: Delay between being infected and recovering
+    :param Tih: Delay between being infected and being sent to gospital
     :param exposed:
     :param infected:
     :param new_infected:
@@ -186,12 +186,12 @@ def recover_day(time: int, Kir: float, Khr: float, Ker: float,
                 recovered: [float], new_infected: [float], new_hospitalized: [float], new_exit_intensive: [float]) -> float:
     """
     Computes the volume of people who recover at day time
-    :param time:
-    :param Kir:
-    :param Khr:
-    :param Ker:
-    :param Tir:
-    :param Thr:
+    :param time: Day after start day the simulation is currently at
+    :param Kir: Coefficient from infected to recovered
+    :param Khr: Coefficient from hospitalized to recovered
+    :param Ker: Coefficient from exit intensive to recovered
+    :param Tir: Delay between being infected and recovering
+    :param Thr: Delay between being sent to gospital and recovering
     :param recovered:
     :param new_infected:
     :param new_hospitalized:
@@ -211,13 +211,13 @@ def hospitalize_day(time: int, Kih: float, Khic: float, Khr: float,
                     hospitalized: [float], new_infected: [float], new_hospitalized: [float]) -> float:
     """
     Computes the volume of people who are hospitalized at day time
-    :param time:
-    :param Kih:
-    :param Khic:
-    :param Khr:
-    :param Tih:
-    :param Thic:
-    :param Thr:
+    :param time: Day after start day the simulation is currently at
+    :param Kih: Coefficient from infected to hospitalized
+    :param Khic: Coefficient from hospitalized to intensive care
+    :param Khr: Coefficient from hospitalized to recovered
+    :param Tih: Delay between being infected and being sent to gospital
+    :param Thic: Delay between being sent to gospital and then sent to intensive care
+    :param Thr: Delay between being sent to gospital and recovering
     :param hospitalized:
     :param new_infected:
     :param new_hospitalized:
@@ -236,10 +236,10 @@ def intensive_day(time: int, Khic: float,
                   new_intensive_care: [float], new_hospitalized: [float]) -> float:
     """
     Computes the volume of people in intensive care during day time
-    :param time:
-    :param Khic:
-    :param Thic:
-    :param Tice:
+    :param time: Day after start day the simulation is currently at
+    :param Khic: Coefficient from hospitalized to intensive care
+    :param Thic: Delay between being sent to gospital and then sent to intensive care
+    :param Tice: Delay between entering and exiting intensive care services
     :param new_intensive_care:
     :param new_hospitalized:
     :return:
@@ -255,10 +255,10 @@ def exi_day(time: int, Ker: float, Ked: float,
             new_intensive_care: [float], exit_intensive: [float]) -> float:
     """
     Computes the number of people who exit intensive care during day time
-    :param time:
-    :param Ker:
-    :param Ked:
-    :param Tice:
+    :param time: Day after start day the simulation is currently at
+    :param Ker: Coefficient from exit intensive to recovered
+    :param Ked: Coefficient from exit intensive to dead
+    :param Tice: Delay between entering and exiting intensive care services
     :param new_intensive_care:
     :param exit_intensive:
     :return:
@@ -273,7 +273,7 @@ def exi_day(time: int, Ker: float, Ked: float,
 def death_day(Ked: float, dead: [float], exit_intensive: [float]) -> float:
     """
     Computes the volume of dead people
-    :param Ked:
+    :param Ked: Coefficient from exit intensive to dead
     :param dead:
     :param exit_intensive:
     :return:
@@ -289,9 +289,9 @@ def exposed_to_infected(time: int, Kei: float, Tei: int,
                         exposed: [float], infected: [float]) -> float:
     """
     Computes the variation between exposed and infected
-    :param time:
-    :param Kei:
-    :param Tei:
+    :param time: Day after start day the simulation is currently at
+    :param Kei: Coefficient from exposed to infected
+    :param Tei: Delay between being infected and infecting others
     :param exposed: history of esposed people so far
     :param infected: history of infected people so far
     :return:
@@ -305,9 +305,9 @@ def exposed_to_infected(time: int, Kei: float, Tei: int,
 def recover_1(time: int, Kir: float, Tir: int, new_infected: [float]) -> float:
     """
     Computes the variation between infected and recovered
-    :param time:
-    :param Kir:
-    :param Tir:
+    :param time: Day after start day the simulation is currently at
+    :param Kir: Coefficient from infected to recovered
+    :param Tir: Delay between being infected and recovering
     :param new_infected:
     :return:
     """
@@ -320,9 +320,9 @@ def recover_1(time: int, Kir: float, Tir: int, new_infected: [float]) -> float:
 def recover_2(time, Khr, Thr, new_hospitalized):
     """
     Computes the variation between hospitalized and recovered
-    :param time:
-    :param Khr:
-    :param Thr:
+    :param time: Day after start day the simulation is currently at
+    :param Khr: Coefficient from hospitalized to recovered
+    :param Thr: Delay between being sent to gospital and recovering
     :param new_hospitalized:
     :return:
     """
@@ -335,7 +335,7 @@ def recover_2(time, Khr, Thr, new_hospitalized):
 def recover_3(Ker, new_exit_intensive):
     """
     Computes the variation between exit of intensive care and recovered
-    :param Ker:
+    :param Ker: Coefficient from exit intensive to recovered
     :param new_exit_intensive:
     :return:
     """
@@ -345,9 +345,9 @@ def recover_3(Ker, new_exit_intensive):
 def infected_to_hospitalized(time, Kih, Tih, new_infected):
     """
     Computes the variation between infected and hospitalized
-    :param time:
-    :param Kih:
-    :param Tih:
+    :param time: Day after start day the simulation is currently at
+    :param Kih: Coefficient from infected to hospitalized
+    :param Tih: Delay between being infected and being sent to gospital
     :param new_infected:
     :return:
     """
@@ -361,9 +361,9 @@ def infected_to_hospitalized(time, Kih, Tih, new_infected):
 def hospitalized_to_intensive(time, Khic, Thic, new_hospitalized):
     """
     Computes the variation between hospitalized and intensive care
-    :param time:
-    :param Khic:
-    :param Thic:
+    :param time: Day after start day the simulation is currently at
+    :param Khic: Coefficient from hospitalized to intensive care
+    :param Thic: Delay between being sent to gospital and then sent to intensive care
     :param new_hospitalized:
     :return:
     """
@@ -376,8 +376,8 @@ def hospitalized_to_intensive(time, Khic, Thic, new_hospitalized):
 def intensive_to_exi(time, Tice, new_intensive_care):
     """
     Computes the variation between intensive care and intensive care exit
-    :param time:
-    :param Tice:
+    :param time: Day after start day the simulation is currently at
+    :param Tice: Delay between entering and exiting intensive care services
     :param new_intensive_care:
     :return:
     """
@@ -390,7 +390,7 @@ def intensive_to_exi(time, Tice, new_intensive_care):
 def exi_to_death(Ked, new_exit_intensive):
     """
     Computes the variation between intensive care exit and daths
-    :param Ked:
+    :param Ked: Coefficient from exit intensive to dead
     :param new_exit_intensive:
     :return:
     """
