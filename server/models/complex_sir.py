df55ffaa795df2d57d4d1884784f8f8b1206b789
Paul54460 <paul.festor@gmail.com>
2020-04-05 22:14:05 +0200

    Clarification du code
    Ajout de typage et commentaires. les descriptions de fonctions doivent être complètés et le code retesté.

--- server/models/complex_sir.py
+++ server/models/complex_sir.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 def complex_sir_v1(population: int,
                    Kpe: float, Kei: float, Kir: float, Kih: float, Khic: float, Khr: float, Ked: float, Ker: float,
                    Tei: int, Tir: int, Tih: int, Thr: int, Thic: int, Tice: int,
-                   lim_time: int):
+                   lim_time: int) -> {str: [float]}:
     """
     This function computes the predictions according to a complex SIR model
     :param population: Total population size
@@ -49,29 +49,58 @@ def complex_sir_v1(population: int,
                         new_infected, new_hospitalized, new_intensive_care, new_exit_intensive, Kpe, Kei, Kir, Kih,
                         Khic, Khr, Ked, Ker, Tei, Tir, Tih, Thr, Thic, Tice, time)
 
-        recovered = day[0]
-        exposed = day[1]
-        infected = day[2]
-        hospitalized = day[3]
-        intensive_care = day[4]
-        exit_intensive = day[5]
-        dead = day[6]
+        recovered = day["recovered"]
+        exposed = day["exposed"]
+        infected = day["infected"]
+        hospitalized = day["hospitalized"]
+        intensive_care = day["intensive_care"]
+        exit_intensive = day["exit"]
+        dead = day["dead"]
 
-        new_infected = day[7]
-        new_hospitalized = day[8]
-        new_intensive_care = day[9]
-        new_exit_intensive = day[10]
+        new_infected = day["new_infected"]
+        new_hospitalized = day["new_hospitalized"]
+        new_intensive_care = day["new_intensive"]
+        new_exit_intensive = day["new_exit"]
 
-    return recovered, exposed, infected, dead, hospitalized, intensive_care, exit_intensive
+    return {"recovered": recovered, "exposed": exposed, "infected": infected, "dead": dead, "hospitalized": dead,
+            "intensive_care": intensive_care, "exit_intensive": exit_intensive, "exit_intensive": exit_intensive}
 
 
-def model_day(recovered, exposed, infected, hospitalized, intensive_care, exit_intensive, dead,
-              new_infected, new_hospitalized, new_intensive_care, new_exit_intensive, Kei, Kir, Kih, Khic, Khr,
-              Ked, Ker, Tei, Tir, Tih, Thr, Thic, Tice, time):
+def model_day(recovered: [float], exposed: [float], infected: [float], hospitalized: [float], intensive_care: [float],
+              exit_intensive: [float], dead: [float],
+              new_infected: [float], new_hospitalized: [float], new_intensive_care: [float], new_exit_intensive: [float],
+              Kei: float, Kir: float, Kih: float, Khic: float, Khr: float, Ked: float, Ker: float,
+              Tei: int, Tir: int, Tih: int, Thr: int, Thic: int, Tice: int,
+              time: int) -> {str: [float]}:
     """
-    Fonction qui fait évoluer le modèle sur une journée
+    Models a day od simulation
+    :param recovered:
+    :param exposed:
+    :param infected:
+    :param hospitalized:
+    :param intensive_care:
+    :param exit_intensive:
+    :param dead:
+    :param new_infected:
+    :param new_hospitalized:
+    :param new_intensive_care:
+    :param new_exit_intensive:
+    :param Kei:
+    :param Kir:
+    :param Kih:
+    :param Khic:
+    :param Khr:
+    :param Ked:
+    :param Ker:
+    :param Tei:
+    :param Tir:
+    :param Tih:
+    :param Thr:
+    :param Thic:
+    :param Tice:
+    :param time:
+    :return:
     """
-
     # On copie les listes pour ne pas avoir de problème
     recover = deepcopy(recovered)
     expose = deepcopy(exposed)
@@ -104,14 +133,20 @@ def model_day(recovered, exposed, infected, hospitalized, intensive_care, exit_i
     # print(new_exit_intensive)
     death.append(death_day(dead, new_exit_intensive, Ked))
 
-    return recover, expose, infect, hospitalize, intensive, exi, death, new_infect, new_hospitalize, new_intensive, new_exi
+    return {"recovered": recover, "exposed": expose, "infected": infect, "hospitalized": hospitalize,
+            "intensive": intensive, "exit": exi, "dead": death, "new_infected": new_infect,
+            "new_hospitalized": new_hospitalize, "new_intensive": new_intensive_care, "new_exit": new_exi}
 
 
-#
-
-def expose_day(time, Kei, Tei, exposed, infected):
+def expose_day(time: int, Kei: float, Tei: int, exposed: [float], infected: [float]) -> float:
     """
-    Calcule le volume de gens exposés
+    Computes the volume of people who are exposed at day time
+    :param time:
+    :param Kei:
+    :param Tei:
+    :param exposed:
+    :param infected:
+    :return: Volume of the exposed population
     """
 
     e1 = exposed_to_infected(time, Kei, Tei, exposed, infected)
@@ -121,9 +156,22 @@ def expose_day(time, Kei, Tei, exposed, infected):
 
 #
 
-def infect_day(time, Kei, Kir, Kih, Tei, Tir, Tih, exposed, infected, new_infected):
+def infect_day(time: int, Kei: float, Kir: float, Kih: float,
+               Tei: int, Tir: int, Tih: int,
+               exposed: [float], infected: [float], new_infected: [float]) -> float:
     """
-    Calcule le volume de gens infectés
+    Computes the volume of people who are infected at day time
+    :param time:
+    :param Kei:
+    :param Kir:
+    :param Kih:
+    :param Tei:
+    :param Tir:
+    :param Tih:
+    :param exposed:
+    :param infected:
+    :param new_infected:
+    :return:
     """
 
     i1 = exposed_to_infected(time, Kei, Tei, exposed, infected)
@@ -133,9 +181,22 @@ def infect_day(time, Kei, Kir, Kih, Tei, Tir, Tih, exposed, infected, new_infect
     return infected[-1] + i1 - i2 - i3
 
 
-def recover_day(Tir, Thr, Kir, Khr, Ker, time, recovered, new_infected, new_hospitalized, new_exit_intensive):
+def recover_day(time: int, Kir: float, Khr: float, Ker: float,
+                Tir: int, Thr: int,
+                recovered: [float], new_infected: [float], new_hospitalized: [float], new_exit_intensive: [float]) -> float:
     """
-    Calcule le volume de gens guéris
+    Computes the volume of people who recover at day time
+    :param time:
+    :param Kir:
+    :param Khr:
+    :param Ker:
+    :param Tir:
+    :param Thr:
+    :param recovered:
+    :param new_infected:
+    :param new_hospitalized:
+    :param new_exit_intensive:
+    :return:
     """
 
     r1 = recover_1(time, Kir, Tir, new_infected)
@@ -145,9 +206,22 @@ def recover_day(Tir, Thr, Kir, Khr, Ker, time, recovered, new_infected, new_hosp
     return recovered[-1] + r1 + r2 + r3
 
 
-def hospitalize_day(time, Kih, Khic, Khr, Tih, Thic, Thr, hospitalized, new_infected, new_hospitalized):
+def hospitalize_day(time: int, Kih: float, Khic: float, Khr: float,
+                    Tih: int, Thic: int, Thr: int,
+                    hospitalized: [float], new_infected: [float], new_hospitalized: [float]) -> float:
     """
-    Calcule le volume de gens hospitalisés
+    Computes the volume of people who are hospitalized at day time
+    :param time:
+    :param Kih:
+    :param Khic:
+    :param Khr:
+    :param Tih:
+    :param Thic:
+    :param Thr:
+    :param hospitalized:
+    :param new_infected:
+    :param new_hospitalized:
+    :return:
     """
 
     h1 = infected_to_hospitalized(time, Kih, Tih, new_infected)
@@ -157,14 +231,38 @@ def hospitalize_day(time, Kih, Khic, Khr, Tih, Thic, Thr, hospitalized, new_infe
     return hospitalized[-1] + h1 - h2 - h3
 
 
-def intensive_day(time, Khic, Thic, Tice, new_intensive_care, new_hospitalized):
+def intensive_day(time: int, Khic: float,
+                  Thic: int, Tice: int,
+                  new_intensive_care: [float], new_hospitalized: [float]) -> float:
+    """
+    Computes the volume of people in intensive care during day time
+    :param time:
+    :param Khic:
+    :param Thic:
+    :param Tice:
+    :param new_intensive_care:
+    :param new_hospitalized:
+    :return:
+    """
     i1 = hospitalized_to_intensive(time, Khic, Thic, new_hospitalized)
     i2 = intensive_to_exi(time, Tice, new_intensive_care)
 
     return new_intensive_care[-1] + i1 - i2
 
 
-def exi_day(time, Ker, Ked, Tice, new_intensive_care, exit_intensive):
+def exi_day(time: int, Ker: float, Ked: float,
+            Tice: int,
+            new_intensive_care: [float], exit_intensive: [float]) -> float:
+    """
+    Computes the number of people who exit intensive care during day time
+    :param time:
+    :param Ker:
+    :param Ked:
+    :param Tice:
+    :param new_intensive_care:
+    :param exit_intensive:
+    :return:
+    """
     e1 = intensive_to_exi(time, Tice, new_intensive_care)
     e2 = recover_3(Ker, exit_intensive)
     e3 = exi_to_death(Ked, exit_intensive)
@@ -172,9 +270,13 @@ def exi_day(time, Ker, Ked, Tice, new_intensive_care, exit_intensive):
     return exit_intensive[-1] + e1 - e2 - e3
 
 
-def death_day(dead, exit_intensive, Ked):
+def death_day(Ked: float, dead: [float], exit_intensive: [float]) -> float:
     """
-    Calcule le volume de gens morts
+    Computes the volume of dead people
+    :param Ked:
+    :param dead:
+    :param exit_intensive:
+    :return:
     """
     d = exi_to_death(Ked, exit_intensive)
 
@@ -183,20 +285,31 @@ def death_day(dead, exit_intensive, Ked):
 
 ####
 
-def exposed_to_infected(time, Kei, Tei, exposed, infected):
+def exposed_to_infected(time: int, Kei: float, Tei: int,
+                        exposed: [float], infected: [float]) -> float:
     """
-    Calcule la variation entre exposed et infected
+    Computes the variation between exposed and infected
+    :param time:
+    :param Kei:
+    :param Tei:
+    :param exposed: history of esposed people so far
+    :param infected: history of infected people so far
+    :return:
     """
-
     if time < Tei:
         return 0
     else:
         return Kei * (exposed[-1] * infected[-Tei]) / exposed[0]
 
 
-def recover_1(time, Kir, Tir, new_infected):
+def recover_1(time: int, Kir: float, Tir: int, new_infected: [float]) -> float:
     """
-    Calcule la variation entre infected et recovered
+    Computes the variation between infected and recovered
+    :param time:
+    :param Kir:
+    :param Tir:
+    :param new_infected:
+    :return:
     """
     if time < Tir:
         return 0
@@ -206,7 +319,12 @@ def recover_1(time, Kir, Tir, new_infected):
 
 def recover_2(time, Khr, Thr, new_hospitalized):
     """
-    Calcule la variation entre hospitalized et recovered
+    Computes the variation between hospitalized and recovered
+    :param time:
+    :param Khr:
+    :param Thr:
+    :param new_hospitalized:
+    :return:
     """
     if time < Thr:
         return 0
@@ -216,14 +334,22 @@ def recover_2(time, Khr, Thr, new_hospitalized):
 
 def recover_3(Ker, new_exit_intensive):
     """
-    Calcule la variation entre exit_intensive et recovered
+    Computes the variation between exit of intensive care and recovered
+    :param Ker:
+    :param new_exit_intensive:
+    :return:
     """
     return Ker * new_exit_intensive[-1]
 
 
 def infected_to_hospitalized(time, Kih, Tih, new_infected):
     """
-    Calcule la variation entre infected et hospitalized
+    Computes the variation between infected and hospitalized
+    :param time:
+    :param Kih:
+    :param Tih:
+    :param new_infected:
+    :return:
     """
 
     if time < Tih:
@@ -234,7 +360,12 @@ def infected_to_hospitalized(time, Kih, Tih, new_infected):
 
 def hospitalized_to_intensive(time, Khic, Thic, new_hospitalized):
     """
-    Calcule la variation entre hospitalized et intensive
+    Computes the variation between hospitalized and intensive care
+    :param time:
+    :param Khic:
+    :param Thic:
+    :param new_hospitalized:
+    :return:
     """
     if time < Thic:
         return 0
@@ -243,6 +374,13 @@ def hospitalized_to_intensive(time, Khic, Thic, new_hospitalized):
 
 
 def intensive_to_exi(time, Tice, new_intensive_care):
+    """
+    Computes the variation between intensive care and intensive care exit
+    :param time:
+    :param Tice:
+    :param new_intensive_care:
+    :return:
+    """
     if time < Tice:
         return 0
     else:
@@ -250,4 +388,10 @@ def intensive_to_exi(time, Tice, new_intensive_care):
 
 
 def exi_to_death(Ked, new_exit_intensive):
+    """
+    Computes the variation between intensive care exit and daths
+    :param Ked:
+    :param new_exit_intensive:
+    :return:
+    """
     return Ked * new_exit_intensive[-1]
