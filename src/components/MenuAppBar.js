6ae13de6903c2013afe13e98782c6e9c99aaee76
Paul54460 <paul.festor@gmail.com>
2020-04-05 10:15:32 +0200

    Ajout de navigation via la AppBar
    Il faudra maintenant lier les bonnes pages au lieu des placeholders que j'ai actuellement.

--- src/components/MenuAppBar.js
+++ src/components/MenuAppBar.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -16,18 +17,34 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+function TabContainer(props) {
+    return (
+        <Typography component="div" style={{ padding: 8 * 3 }}>
+            {props.children}
+        </Typography>
+    );
+}
+
 export const NavigationAppBar = () => {
     const classes = useStyles();
+    const [state, setState] = React.useState({ value: 0 });
+
+    const handleChange = (event, value) => {
+        setState({ value });
+    };
 
     return (
         <div className={classes.root}>
             <AppBar position="static" title="My App">
-                <Tabs>
+                <Tabs value={state.value} onChange={handleChange}>
                     <Tab label="Accueil" />
                     <Tab label="Modèle SIR simple" />
                     <Tab label="Modèle complexe" />
                 </Tabs>
             </AppBar>
+            {state.value === 0 && <TabContainer>Afficher page d'accueil</TabContainer>}
+            {state.value === 1 && <TabContainer>Afficher page modele SIR</TabContainer>}
+            {state.value === 2 && <TabContainer>Afficher page modele complexe</TabContainer>}
         </div>
     );
 };
