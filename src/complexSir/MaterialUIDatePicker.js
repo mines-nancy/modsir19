7006a9d9dc78a966b02ce3f3bd64a44570609fb5
Paul54460 <paul.festor@gmail.com>
2020-04-11 09:33:57 +0200

    More modifications, still not working

--- src/complexSir/MaterialUIDatePicker.js
+++ src/complexSir/MaterialUIDatePicker.js
@@ -1,16 +1,17 @@
 import 'date-fns';
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import DateFnsUtils from '@date-io/date-fns';
 import {
     MuiPickersUtilsProvider,
     KeyboardTimePicker,
     KeyboardDatePicker,
 } from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
+import DateFnsUtils from '@date-io/date-fns';
 
-export default function MaterialUIPickers() {
+export const MaterialUIPickers = () => {
     // The first commit of Material-UI
-    const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+    const [selectedDate, setSelectedDate] = React.useState(new Date(2020, 0, 24));
 
     const handleDateChange = (date) => {
         setSelectedDate(date);
@@ -30,27 +31,6 @@ export default function MaterialUIPickers() {
                     onChange={handleDateChange}
                     KeyboardButtonProps={{
                         'aria-label': 'change date',
-                    }}
-                />
-                <KeyboardDatePicker
-                    margin="normal"
-                    id="date-picker-dialog"
-                    label="Date picker dialog"
-                    format="MM/dd/yyyy"
-                    value={selectedDate}
-                    onChange={handleDateChange}
-                    KeyboardButtonProps={{
-                        'aria-label': 'change date',
-                    }}
-                />
-                <KeyboardTimePicker
-                    margin="normal"
-                    id="time-picker"
-                    label="Time picker"
-                    value={selectedDate}
-                    onChange={handleDateChange}
-                    KeyboardButtonProps={{
-                        'aria-label': 'change time',
                     }}
                 />
             </Grid>
