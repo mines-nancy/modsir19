b0f1a1f98cd1c929accce0a77e9e0490ed44ea6c
pem <pierre-etienne.moreau@loria.fr>
2020-04-13 11:25:11 +0200

    introduction des box dms

--- server/models/sir_h/box.py
+++ server/models/sir_h/box.py
@@ -1,5 +1,4 @@
 import math
-from collections import deque
 
 
 class Box:
@@ -8,8 +7,7 @@ class Box:
         self._duration = duration
         self._capacity = capacity
 
-        self._size = 0
-        self._queue = deque()  # items in the box
+        self._size = 0  # dms model
         self._input = 0  # number of inputs
         self._output = 0  # number of outputs
 
@@ -17,7 +15,6 @@ class Box:
         input = round(self._input, 2)
         output = round(self._output, 2)
         size = round(self._size, 2)
-        queue = f'{[round(x,2) for x in self._queue]}'
         return f'{self._name}[{input}]\{size}/[{output}]'
 
     def step(self):
@@ -27,19 +24,17 @@ class Box:
             self._output += input
             return
 
-        if len(self._queue) == self._duration:
-            output = self._queue.pop()
-            # print(f'pop: {output}')
-            self._size -= output
-            self._output += output
-            # print(f'output: {self._output}')
+        output = self._size / self._duration
+        # print(f'pop: {output}')
+        self._size -= output
+        self._output += output
+        # print(f'output: {self._output}')
+
         input = min(self._input, self._capacity-self._size)
         self._input -= input
         self._size += input
-        self._queue.appendleft(input)
 
     def size(self):
-        # return sum([x for x in self._queue])
         return self._size
 
     def full_size(self):
---
+++ server/models/sir_h/state.py
@@ -0,0 +1,135 @@
+from models.sir_h.box import Box
+from models.history import History
+
+
+class State:
+    def __init__(self,
+                 kpe: float,
+                 kem: float,
+                 kmg: float,
+                 kmh: float,
+                 khr: float,
+                 khg: float,
+                 krd: float,
+                 krg: float,
+                 tem: int,
+                 tmg: int,
+                 tmh: int,
+                 thg: int,
+                 thr: int,
+                 trsr: int,
+                 time,
+                 population
+                 ):
+
+        self._delays = {
+            'tem': tem,
+            'tmg': tmg,
+            'tmh': tmh,
+            'thg': thg,
+            'thr': thr
+        }
+
+        self._coefficients = {
+            'kpe': kpe,
+            'kem': kem,
+            'kmg': kmg,
+            'kmh': kmh,
+            'khr': khr,
+            'khg': khg,
+            'krd': krd,
+            'krg': krg,
+        }
+
+        self._boxes = {
+            'S': Box('S', 0),
+            'MG': Box('MG', tmg),
+            'MH': Box('MH', tmh),
+            'G': Box('G'),
+            'HG': Box('HG', thg),
+            'HR': Box('HR', thr),
+            'R': Box('R', 8),
+            'D': Box('D')
+        }
+
+        # src -> [targets]
+        self._moves = {
+            'MG': [('G', 1)],
+            'MH': [('HG', khg), ('HR', khr)],
+            'HG': [('G', 1)],
+            'HR': [('R', 1)],
+            'R': [('G', krg), ('D', krd)],
+        }
+
+        self.time = time
+
+        self.e0 = kpe*population
+        self.box('E').add(self.e0)
+        self.box('MH').add(1)
+
+    def boxes(self):
+        return self._boxes.values()
+
+    def boxnames(self):
+        return self._boxes.keys()
+
+    def box(self, name):
+        return self._boxes[name]
+
+    def output(self, name):
+        return self.box(name).output()
+
+    def delay(self, name):
+        return self._delays[name]
+
+    def coefficient(self, name):
+        return self._coefficients[name]
+
+    def __str__(self):
+        pop = sum([box.full_size() for box in self.boxes()])
+        return f'{self.box("E")} {self.box("MG")} {self.box("MH")} {self.box("HG")} {self.box("HR")} {self.box("R")} {self.box("G")} {self.box("D")} POP={round(pop,2)}'
+
+    def get_time0(self):
+        return 0
+
+    def move(self, src_name, dest_name, delta):
+        self.box(src_name).remove(delta)
+        self.box(dest_name).add(delta)
+
+    def step(self, history):
+        self.time += 1
+        for box in self.boxes():
+            box.step()
+
+        self.step_exposed(history)
+        self.generic_steps(self._moves)
+
+    def generic_steps(self, moves):
+        for src_name in moves.keys():
+            for dest_name, coefficient in moves[src_name]:
+                self.move(src_name, dest_name, coefficient *
+                          self.output(src_name))
+
+    def step_exposed(self, history):
+        previous_state = history.get_last_state(self.time - 1)
+        state_tem = history.get_last_state(self.time - (1+self.delay('tem')))
+        if state_tem == None or previous_state == None:
+            return
+
+        infected_size = state_tem.box('MG').size() + state_tem.box('MH').size()
+        delta = self.coefficient('kem') * \
+            (previous_state.box('E').output() * infected_size) / self.e0
+        self.move('E', 'MG', self.coefficient('kmg') * delta)
+        self.move('E', 'MH', self.coefficient('kmh') * delta)
+
+    def extract_series(self, history):
+        series = {'E': ['E'], 'G': ['G'], 'M': ['MG', 'MH'],
+                  'H': ['HG', 'HR'], 'D': ['D'], 'R': ['R']}
+        # sum the sizes of boxes
+        lists = {name: [] for name in series.keys()}
+        for state in history.sorted_list():
+            sizes = {name: state.box(name).full_size()
+                     for name in self.boxnames()}
+            for name in lists.keys():
+                lists[name].append(sum([sizes[n] for n in series[name]]))
+        return lists['G'], lists['E'], lists['M'], lists['D'], lists['H'], lists['R'], []
