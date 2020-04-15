f2aed40f9e4e14eba16566eb7094a8dfc8bbbdc9
pem <pierre-etienne.moreau@loria.fr>
2020-04-15 10:03:57 +0200

    use pick

--- src/complexSir/ComplexSIRDownloadButton.js
+++ src/complexSir/ComplexSIRDownloadButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { useTranslate } from 'react-polyglot';
 import Button from '@material-ui/core/Button';
-import { zip } from 'lodash';
+import { zip, pick } from 'lodash';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -13,17 +13,6 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const generateCSV = function (values) {
-    const {
-        recovered,
-        exposed,
-        infected,
-        dead,
-        hospitalized,
-        intensive_care,
-        exit_intensive_care,
-        j_0,
-    } = values;
-
     const headers = [
         'exposed',
         'infected',
@@ -33,17 +22,8 @@ const generateCSV = function (values) {
         'recovered',
         'dead',
     ];
-
-    const series = [
-        exposed,
-        infected,
-        hospitalized,
-        intensive_care,
-        exit_intensive_care,
-        recovered,
-        dead,
-    ];
-    const rows = zip(...series);
+    const series = pick(values, headers);
+    const rows = zip(...Object.values(series));
     const csvRows = [headers.join(','), ...rows.map((r) => r.join(','))];
     return csvRows.join('\n');
 };
