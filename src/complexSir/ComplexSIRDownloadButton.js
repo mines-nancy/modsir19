c8e1f5a4c4cda3439c3d969c3dc2e9795fc19f99
Paul54460 <paul.festor@gmail.com>
2020-04-16 09:58:21 +0200

    Adaptation format Polytech

--- src/complexSir/ComplexSIRDownloadButton.js
+++ src/complexSir/ComplexSIRDownloadButton.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { useTranslate } from 'react-polyglot';
 import Button from '@material-ui/core/Button';
 import { zip, pick } from 'lodash';
+import { generateDates } from '../utils/dateGenerator';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -14,6 +15,8 @@ const useStyles = makeStyles((theme) => ({
 
 const generateCSV = function (values) {
     const headers = [
+        'jour',
+        'date',
         'exposed',
         'infected',
         'hospitalized',
@@ -22,9 +25,31 @@ const generateCSV = function (values) {
         'recovered',
         'dead',
     ];
-    const series = pick(values, headers);
-    const rows = zip(...Object.values(series));
-    const csvRows = [headers.join(','), ...rows.map((r) => r.join(','))];
+    const d = new Date(2020, 11, 12).getDate();
+    // eslint-disable-next-line no-console
+    console.log(d);
+    // eslint-disable-next-line no-console
+    // console.log(headers);
+    const csvRows = ['model_name;SIR+H', 'model_version;1.0', headers.join(';')];
+    const j_0 = new Date(2020, 2, 3);
+    const dateRange = generateDates(j_0, values.exposed.length);
+    for (let itr = 0; itr < values.exposed.length; itr++) {
+        csvRows.push(
+            [
+                itr,
+                dateRange[itr],
+                values.exposed[itr],
+                values.infected[itr],
+                values.hospitalized[itr],
+                values.intensive_care[itr],
+                values.exit_intensive_care[itr],
+                values.recovered[itr],
+                values.dead[itr],
+            ]
+                .join(';')
+                .replace(/\./g, ','),
+        );
+    }
     return csvRows.join('\n');
 };
 
