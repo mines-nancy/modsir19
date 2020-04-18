c960692eb87713ed57c72a57828eec8e50b28146
Kmaschta <kevin@marmelab.com>
2020-04-18 15:03:45 +0200

    Last parameters fields

--- src/components/Graph/Edge.js
+++ src/components/Graph/Edge.js
@@ -1,7 +1,13 @@
 import { useEffect } from 'react';
 import LeaderLine from 'leader-line';
 
-export const Edge = ({ ref1, ref2, options = {} }) => {
+export const Edge = ({
+    ref1,
+    ref2,
+    options = {},
+    anchorStartOptions = {},
+    anchorEndOptions = {},
+}) => {
     let line;
 
     useEffect(() => {
@@ -10,8 +16,14 @@ export const Edge = ({ ref1, ref2, options = {} }) => {
         if (ref1 && ref2) {
             timeout = setTimeout(() => {
                 line = new LeaderLine(
-                    LeaderLine.pointAnchor(ref1, { x: '50%', y: '100%' }),
-                    LeaderLine.pointAnchor(ref2, { x: '50%', y: 0 }),
+                    LeaderLine.pointAnchor(ref1, {
+                        x: anchorStartOptions.x || '50%',
+                        y: anchorStartOptions.y || '100%',
+                    }),
+                    LeaderLine.pointAnchor(ref2, {
+                        x: anchorEndOptions.x || '50%',
+                        y: anchorEndOptions.y || 0,
+                    }),
                 );
 
                 line.setOptions({ startSocket: 'bottom', endSocket: 'top', size: 3, ...options });
