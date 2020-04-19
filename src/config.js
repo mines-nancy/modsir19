717d862228f37ed177cd5719afef93760fdd6bdf
Kmaschta <kevin@marmelab.com>
2020-04-19 12:57:53 +0200

    Deployment configuration

--- src/config.js
+++ src/config.js
@@ -1,11 +1,9 @@
 const prod = process.env.NODE_ENV === 'production'; // Anything else is treated as 'dev'
 
-export const API_URL = prod
-    ? 'https://simurea.nancyclotep.com/modsir19servertest'
-    : 'http://localhost:5000';
+export const API_URL = prod ? process.env.REACT_APP_API_URL : 'http://localhost:5000';
 
 if (!API_URL) {
-    console.log('No API connection string. Set API_URL environment variable.');
+    throw new Error('No API connection string. Set REACT_APP_API_URL environment variable.');
 }
 
 export const VERSION = '0.1';
