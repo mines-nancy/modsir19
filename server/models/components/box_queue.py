b29a6a2891ef1a838ef675834e81deaf19a69562
pem <pierre-etienne.moreau@loria.fr>
2020-04-19 09:39:10 +0200

    small fix

--- server/models/components/box_queue.py
+++ server/models/components/box_queue.py
@@ -17,10 +17,9 @@ class BoxQueue(Box):
             self.set_output(previous_output + previous_input)
             return
 
-        duration = int(self._duration)
         new_size = previous_size + previous_input
-        if self._t >= duration:
-            new_output = self.input(1+duration)
+        if self._t >= self._duration:
+            new_output = self.input(1+self._duration)
             self.set_output(previous_output + new_output)
             new_size -= new_output
         self.set_size(new_size)
