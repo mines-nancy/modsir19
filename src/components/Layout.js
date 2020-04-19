4ce391adf930d27eebc841faf841edd58dde9af5
Kmaschta <kevin@marmelab.com>
2020-04-19 18:20:12 +0200

    Better Loading

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { MainAppBar } from './appBar/MainAppBar';
 
-const Layout = ({ children }) => (
+const Layout = ({ children, ...props }) => (
     <>
-        <MainAppBar />
+        <MainAppBar {...props} />
         <div style={{ paddingTop: 64 }}>{children}</div>
     </>
 );
