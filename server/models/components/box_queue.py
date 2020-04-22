553c3e01d3500bb76101558d015fe6b0b8908bd4
pem <pierre-etienne.moreau@loria.fr>
2020-04-22 11:55:18 +0200

    new version of box queue with history

--- server/models/components/box_queue.py
+++ server/models/components/box_queue.py
@@ -1,4 +1,5 @@
 import math
+from collections import deque
 from models.components.box import Box
 
 
@@ -6,6 +7,12 @@ class BoxQueue(Box):
     def __init__(self, name, duration=math.inf):
         Box.__init__(self, name)
         self._duration = duration
+        self._queue = [deque()]  # items in the box
+
+    def queue(self, past=0):
+        if self._t-past >= 0:
+            return self._queue[self._t-past]
+        return deque()
 
     def set_duration(self, value):
         self._duration = value
@@ -14,15 +21,24 @@ class BoxQueue(Box):
         previous_input = self.input()
         previous_output = self.output()
         previous_size = self.size()
+        previous_queue = self.queue()
 
         super().step()
+        self._queue.append(deque(previous_queue))  # copy of previous_queue
+        current_queue = self.queue()
+
         if self._duration == 0:
             self.set_output(previous_output + previous_input)
             return
 
         new_size = previous_size + previous_input
-        if self._t >= self._duration:
-            new_output = self.input(1+self._duration)
+
+        if len(current_queue) == self._duration:
+            new_output = current_queue.pop()
             self.set_output(previous_output + new_output)
             new_size -= new_output
+        current_queue.appendleft(input)
         self.set_size(new_size)
+
+    def get_queue_history(self):
+        return list(self._queue)
