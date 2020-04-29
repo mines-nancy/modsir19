fee7101ab43d9933d33b1367ecd0f915030c97a3
romainpajda <48944361+romainpajda@users.noreply.github.com>
2020-04-29 19:02:18 +0200

    Added test file

---
+++ server/test/test_khi.py
@@ -0,0 +1,24 @@
+from models.components.utils import compute_khi, compute_area_and_expectation
+
+dms = 5
+max_days = 21
+prob_type = 'binomial'
+
+khi_tab = compute_khi(dms, max_days, prob_type)
+residuals = compute_residuals(khi_tab)
+
+area, expectation, sum_khi = compute_area_and_expectation(khi_tab, residuals)
+
+delta_area = abs(dms - area)
+delta_khi = abs(1 - sum_khi)
+delta_expectation = abs(dms - expectation)
+
+print('area = ' + str(area))
+print('delta_area = ' + str(delta_area) + '\n')
+print('expectation = ' + str(expectation))
+print('delta_expectation = ' + str(delta_expectation) + '\n')
+print('sum(khi) = ' + str(sum_khi))
+print('delta_khi = ' + str(delta_khi) + '\n')
+
+plt.plot([k for k in range(max_days + 1)], residuals)
+plt.bar([k for k in range(1, max_days + 1)], khi_tab, color = 'red')
