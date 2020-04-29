2f8815c0bcb3d87cb15218be36941a6d558255e6
pem <pierre-etienne.moreau@loria.fr>
2020-04-29 18:04:06 +0200

    run_simulation as a module

--- server/labs/run_simulator.py
+++ server/labs/run_simulator.py
@@ -1,32 +1,26 @@
-''' this is an ugly hack to be removed and integrated in proper Python package management if ever needed '''
-if __name__ == '__main__' and __package__ is None:
-    import sys
-    from os import path
-    sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
-''' end ugly hack '''
-
+''' This is mainly demo code showing how to invoke the MODSIR19 simulator with
+    default parameters
+    python -m labs.run_simulator [file] tu run the simulator
+'''
 import numpy as np
 import matplotlib.pyplot as plt
-
 from models.sir_h.state import State
 from models.sir_h.simulator import run_sir_h
 from models.rule import RuleChangeField, RuleEvacuation
-import defaults
+from labs.defaults import get_default_params, import_json, export_json
 import argparse
-
-''' This is mainly demo code showing how to invoke the MODSIR19 simulator with
-    default parameters
-'''
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(prog="python run_simulator.py", description='Run MODSIR-19 simulator on provided parameter sets.')
+    parser = argparse.ArgumentParser(
+        prog="python run_simulator.py",
+        description='Run MODSIR-19 simulator on provided parameter sets.')
     parser.add_argument('files', metavar='file', type=str, nargs='*',
-                   help='pathname to parameter set (JSON)')
+                        help='pathname to parameter set (JSON)')
 
     args = parser.parse_args()
 
     ''' default parameters are stored in three distinct groups '''
-    default_params = defaults.get_default_params()
+    default_params = get_default_params()
     parameters = default_params['parameters']
     rules = default_params['rules']
     data = default_params['data']
@@ -39,7 +33,7 @@ if __name__ == "__main__":
     day0 = data['day0']
     data_chu = data['data_chu_rea']
     data_day0 = {date-day0: data_chu[date]
-                 for date in data_chu }
+                 for date in data_chu}
 
     SM = series['SM']
     SI = series['SI']
@@ -48,11 +42,12 @@ if __name__ == "__main__":
 
     plt.plot(x, SM, label="Médecine")
     plt.plot(x, SI, label="Soins Intensifs")
-    plt.plot(list(data_day0.keys()), list(data_day0.values()), 'x',  label="Data CHU")
+    plt.plot(list(data_day0.keys()), list(
+        data_day0.values()), 'x',  label="Data CHU")
     plt.legend(loc='upper right')
 
-    for f in vars(args)['files'] :
-        parameters, rules, other = defaults.import_json(f)
+    for f in vars(args)['files']:
+        parameters, rules, other = import_json(f)
         series = run_sir_h(parameters, rules)
         SI = series['SI']
         plt.plot(x, SI, label="SI " + f)
@@ -60,7 +55,6 @@ if __name__ == "__main__":
     plt.show()
 
     ''' examples for exporting/importing parameters
-    defaults.export_json("mytest.json", parameters, rules, other)
+    export_json("mytest.json", parameters, rules, other)
 
     '''
-
