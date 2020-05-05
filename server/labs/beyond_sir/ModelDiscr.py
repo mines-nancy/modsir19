b4b215d46915e4494ee7a2b892f35e3789aa5d66
LAMIROY Bart <bart.lamiroy@loria.fr>
2020-05-05 14:03:07 +0200

    Creating homogenous execution environment and conditions for differential model and discrete model

--- server/labs/beyond_sir/ModelDiscr.py
+++ server/labs/beyond_sir/ModelDiscr.py
@@ -1,27 +1,44 @@
-def run_model(variables: [float], parameters, rules, **kwargs):
+# -*- coding: utf-8 -*-
+''' SIR+H model based on discrete transitional states '''
 
-    parameters = model_parameters
+import numpy as np
 
-    beta_pre, beta_post, patient0, dm_h = variables
-    #print(f'beta_pre={beta_pre} beta_post={beta_post}')
-    parameters.update({'patient0': patient0,
-                       'beta': beta_pre,
-                       'dm_h': dm_h})
+from models.sir_h.simulator import run_sir_h
+from models.rule import RuleChangeField
+from labs.defaults import get_default_params
+
+def Model2(parameters, rules, **kwargs):
+
+    parameters = dict(parameters)
+    parameters.update(kwargs)
 
     # Changement par rapport à la version de PEM en reprenant l'ensemble des règles du confinement
     confinement = get_default_params()['other']['confinement']
 
-    rules = [RuleChangeField(confinement,  'beta',  beta_post),
+    R0_start = parameters['beta']*parameters['dm_r'] if not 'R0_start' in parameters.keys() else parameters['R0_start']
+    R0_confinement = 0.6 if not 'R0_confinement' in parameters.keys() else parameters['R0_confinement']
+    R0_end = 1.2 if not 'R0_end' in parameters.keys() else parameters['R0_end']
+
+    if not 'beta_post' in parameters.keys() :
+        parameters['beta_post'] = R0_confinement/parameters['dm_r']
+
+    rules = [RuleChangeField(confinement,  'beta',  parameters['beta_post']),
              RuleChangeField(confinement,  'r',  1.0)]
 
     lists = run_sir_h(parameters, rules)
+    t = np.linspace(0, parameters['lim_time'], parameters['lim_time'])
 
-    spike_height = max(lists["SI"])
-    spike_date = lists["SI"].index(spike_height)
+    def R0(t, k = 1.0, R0_start = R0_start,
+        t_confinement = confinement, R0_confinement = R0_confinement,
+        t_end = parameters['lim_time'], R0_end = R0_end):
+        # return 3.31
+        # return R0_start if t < t_confinement else R0_confinement
+        # if t<(t_confinement + t_end)/2:
+        return (R0_start-R0_confinement) / (1 + np.exp(-k*(-t + t_confinement))) + R0_confinement
+        # else:
+          # return (R0_confinement-R0_end) / (1 + np.exp(-k*(-t + t_end))) + R0_end
 
-    return spike_height, spike_date, lists["SI"]
+    R0_over_time = [R0(i) for i in range(len(t))]
 
 
-def Model(parameters, **kwargs):
-
-    return t, SE, INCUB, I, SM, SI, R, DC, R0_over_time
+    return t, lists['SE'], lists['INCUB'], lists['I'], lists['SM'], lists['SI'], lists['R'], lists['DC'], R0_over_time
