c2d09f4eb61bbf3ccb2bbbd02bcb67fc4d560fb8
LAMIROY Bart <bart.lamiroy@loria.fr>
2020-05-05 22:18:21 +0200

    First generic and functional version

--- server/labs/beyond_sir/ModelDiscr.py
+++ server/labs/beyond_sir/ModelDiscr.py
@@ -7,21 +7,34 @@ from models.sir_h.simulator import run_sir_h
 from models.rule import RuleChangeField
 from labs.defaults import get_default_params
 
-def Model2(parameters, rules, **kwargs):
+def model_disc(model_params, **kwargs):
     t_confinement = get_default_params()['other']['confinement']
     t_end = 126
 
-    parameters = dict(parameters)
-    parameters.update(kwargs)
+    parameters = dict(model_params['parameters'])
+    other_arguments = dict(kwargs)
+    parameters.update(other_arguments)
 
-    R0_start = parameters['beta']*parameters['dm_r'] if not 'R0_start' in parameters.keys() else parameters['R0_start']
-    R0_confinement = 0.6 if not 'R0_confinement' in parameters.keys() else parameters['R0_confinement']
-    R0_end = 1.2 if not 'R0_end' in parameters.keys() else parameters['R0_end']
+    if not 'beta_post' in parameters.keys() :
+        parameters['beta_post'] = get_default_params()['other']['r0_confinement']/parameters['dm_r']
+    if not 'beta_end' in parameters.keys() :
+        parameters['beta_end'] = 1.2/parameters['dm_r']
 
+    R0_start = parameters['beta']*parameters['dm_r']
+    R0_confinement = parameters['beta_post']*parameters['dm_r']
+    R0_end = parameters['beta_end']*parameters['dm_r']
+
+    '''
+    beta_0 = parameters['beta'] if not 'R0_start' in parameters.keys() else parameters['R0_start']/parameters['dm_r']
+    beta_post = R0_confinement/parameters['dm_r']
+    beta_end = R0_end/parameters['dm_r']
+
+    parameters['beta'] = beta_0
     if not 'beta_post' in parameters.keys() :
         parameters['beta_post'] = R0_confinement/parameters['dm_r']
     if not 'beta_end' in parameters.keys() :
         parameters['beta_end'] = R0_end/parameters['dm_r']
+    '''
 
     rules = [RuleChangeField(t_confinement,  'beta',  parameters['beta_post']),
              RuleChangeField(t_end,  'beta',  parameters['beta_end']),]
@@ -41,5 +54,4 @@ def Model2(parameters, rules, **kwargs):
 
     R0_over_time = [R0(i) for i in range(len(t))]
 
-
-    return t, lists['SE'], lists['INCUB'], lists['I'], lists['SM'], lists['SI'], lists['R'], lists['DC'], R0_over_time
+    return t, np.array(lists['SE']), np.array(lists['INCUB']), np.array(lists['I']), np.array(lists['SM']), np.array(lists['SI']), np.array(lists['R']), np.array(lists['DC']), R0_over_time
