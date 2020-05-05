e38ff4f9e1647999a106b81ce4c797fcda4be28c
LAMIROY Bart <bart.lamiroy@loria.fr>
2020-05-05 14:24:24 +0200

    Added deconfinement period

--- server/labs/beyond_sir/ModelDiscr.py
+++ server/labs/beyond_sir/ModelDiscr.py
@@ -8,12 +8,11 @@ from models.rule import RuleChangeField
 from labs.defaults import get_default_params
 
 def Model2(parameters, rules, **kwargs):
+    t_confinement = get_default_params()['other']['confinement']
+    t_end = 126
 
     parameters = dict(parameters)
     parameters.update(kwargs)
-
-    # Changement par rapport à la version de PEM en reprenant l'ensemble des règles du confinement
-    confinement = get_default_params()['other']['confinement']
 
     R0_start = parameters['beta']*parameters['dm_r'] if not 'R0_start' in parameters.keys() else parameters['R0_start']
     R0_confinement = 0.6 if not 'R0_confinement' in parameters.keys() else parameters['R0_confinement']
@@ -21,15 +20,17 @@ def Model2(parameters, rules, **kwargs):
 
     if not 'beta_post' in parameters.keys() :
         parameters['beta_post'] = R0_confinement/parameters['dm_r']
+    if not 'beta_end' in parameters.keys() :
+        parameters['beta_end'] = R0_end/parameters['dm_r']
 
-    rules = [RuleChangeField(confinement,  'beta',  parameters['beta_post']),
-             RuleChangeField(confinement,  'r',  1.0)]
+    rules = [RuleChangeField(t_confinement,  'beta',  parameters['beta_post']),
+             RuleChangeField(t_end,  'beta',  parameters['beta_end']),]
 
     lists = run_sir_h(parameters, rules)
     t = np.linspace(0, parameters['lim_time'], parameters['lim_time'])
 
     def R0(t, k = 1.0, R0_start = R0_start,
-        t_confinement = confinement, R0_confinement = R0_confinement,
+        t_confinement = t_confinement, R0_confinement = R0_confinement,
         t_end = parameters['lim_time'], R0_end = R0_end):
         # return 3.31
         # return R0_start if t < t_confinement else R0_confinement
