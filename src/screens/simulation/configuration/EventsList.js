1d8b7d131fbd93056167ef07a589b24ce89c3d0e
Kmaschta <kevin@marmelab.com>
2020-05-11 16:36:12 +0200

    Editable event name

--- src/screens/simulation/configuration/EventsList.js
+++ src/screens/simulation/configuration/EventsList.js
@@ -1,26 +1,18 @@
 import React, { useState } from 'react';
+import { format, startOfDay } from 'date-fns';
 import {
     makeStyles,
     withStyles,
     Button,
     Stepper,
-    Step,
-    StepLabel,
-    StepContent,
     StepConnector as MuiStepConnector,
-    Card,
-    CardHeader,
-    CardContent,
     Typography,
-    Tooltip,
-    IconButton,
     Grid,
 } from '@material-ui/core';
-import { DeleteForever } from '@material-ui/icons';
 
-import NewEventModal, { parameterLabels } from './NewEventModal';
-import { format } from 'date-fns';
-import { startOfDay } from 'date-fns/esm';
+import NewEventModal from './NewEventModal';
+
+import Event from './Event';
 
 const useStyles = makeStyles((theme) => ({
     container: {
@@ -33,23 +25,6 @@ const useStyles = makeStyles((theme) => ({
     stepper: {
         backgroundColor: '#eee',
     },
-    stepIcon: {
-        display: 'flex',
-        height: 22,
-        alignItems: 'center',
-    },
-    stepIconCircle: {
-        width: 10,
-        height: 10,
-        borderRadius: '50%',
-        backgroundColor: theme.palette.primary.main,
-    },
-    stepContent: {
-        marginLeft: 5,
-    },
-    eventChangeCard: {
-        marginBottom: theme.spacing(1),
-    },
     empty: {
         marginTop: theme.spacing(10),
     },
@@ -60,45 +35,6 @@ const StepConnector = withStyles({
         marginLeft: 5,
     },
 })(MuiStepConnector);
-
-const StepIcon = () => {
-    const classes = useStyles();
-
-    return (
-        <div className={classes.stepIcon}>
-            <div className={classes.stepIconCircle} />
-        </div>
-    );
-};
-
-const Event = ({ event, onDelete }) => {
-    const classes = useStyles();
-
-    return (
-        <Step expanded>
-            <StepLabel StepIconComponent={StepIcon}>
-                {format(event.date, 'dd/MM/yyyy')} - {event.name}
-            </StepLabel>
-            <StepContent classes={{ root: classes.stepContent }}>
-                {event.changes.map((change) => (
-                    <Card key={change} className={classes.eventChangeCard}>
-                        <CardHeader
-                            title={<Typography>{parameterLabels[change]}</Typography>}
-                            action={
-                                <Tooltip title="Supprimer l'évènement">
-                                    <IconButton onClick={onDelete(change)}>
-                                        <DeleteForever />
-                                    </IconButton>
-                                </Tooltip>
-                            }
-                        />
-                        <CardContent>{/* TODO: Input */}</CardContent>
-                    </Card>
-                ))}
-            </StepContent>
-        </Step>
-    );
-};
 
 const NewEventButton = (props) => (
     <Button variant="outlined" color="primary" size="small" {...props}>
@@ -184,6 +120,11 @@ const EventsList = ({ events, setEvents, initialDate }) => {
         setEvents(newEvents);
     };
 
+    const handleEventRename = (event) => (name) => {
+        const key = format(event.date, 'yyyy-MM-dd');
+        setEvents({ ...events, [key]: { ...event, name } });
+    };
+
     const eventEntries = Object.entries(events);
     eventEntries.sort(orderByDate);
 
@@ -209,7 +150,12 @@ const EventsList = ({ events, setEvents, initialDate }) => {
                     classes={{ vertical: classes.stepper }}
                 >
                     {eventEntries.map(([key, event]) => (
-                        <Event key={key} event={event} onDelete={handleEventDelete(event)} />
+                        <Event
+                            key={key}
+                            event={event}
+                            onDelete={handleEventDelete(event)}
+                            onRename={handleEventRename(event)}
+                        />
                     ))}
                 </Stepper>
             )}
