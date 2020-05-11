cbc2c5a7fb3296cb204afb68f6f1f56de9d4e801
pem <pierre-etienne.moreau@loria.fr>
2020-05-11 10:02:51 +0200

    first version with final-form

--- src/components/sirPlusH/MixerConvolution.js
+++ src/components/sirPlusH/MixerConvolution.js
@@ -1,31 +1,16 @@
 import React from 'react';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
-import {
-    ExpansionPanel,
-    ExpansionPanelDetails,
-    ExpansionPanelSummary,
-    Grid,
-    Tooltip,
-    Typography,
-    Slider,
-    Input,
-    Button,
-    IconButton,
-} from '@material-ui/core';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { useTranslate } from 'react-polyglot';
-import { SelectFieldWithDate } from './SelectFieldWithDate';
-import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns';
-
-import { Form, Field } from 'react-final-form';
+import { IconButton, createStyles, makeStyles } from '@material-ui/core';
 import RemoveCoefficient from '@material-ui/icons/RemoveOutlined';
 import AddCoefficient from '@material-ui/icons/AddOutlined';
-import ProportionField from '../fields/ProportionField';
-import AutoSave from '../fields/AutoSave';
+
+import { Form, Field } from 'react-final-form';
 import createDecorator from 'final-form-calculate';
 import arrayMutators from 'final-form-arrays';
 import { FieldArray } from 'react-final-form-arrays';
+
+import VerticalProportionField from './VerticalProportionField';
+import ProportionField from '../fields/ProportionField';
+import AutoSave from '../fields/AutoSave';
 
 const useStyles = makeStyles((theme) =>
     createStyles({
@@ -38,6 +23,9 @@ const useStyles = makeStyles((theme) =>
         },
         input: {
             width: 50,
+        },
+        verticalSlider: {
+            height: 200,
         },
         slider: {
             width: 60,
@@ -53,76 +41,7 @@ const useStyles = makeStyles((theme) =>
     }),
 );
 
-const valuetext = (value) => `${value}`;
-const VerticalSlider = ({ index, value, min, max, step, onSliderChange }) => {
-    const classes = useStyles();
-
-    const marks = [
-        {
-            value: 0,
-            label: '0%',
-        },
-        {
-            value: 20,
-        },
-        {
-            value: 100,
-            label: '100%',
-        },
-    ];
-    return (
-        <Slider
-            orientation="vertical"
-            className={classes.slider}
-            index={index}
-            value={value}
-            min={min}
-            max={max}
-            step={step}
-            onChange={(event, newValue) => {
-                onSliderChange(event, newValue, index);
-            }}
-            aria-labelledby="input-slider"
-            valueLabelDisplay="auto"
-            getAriaValueText={valuetext}
-        />
-    );
-};
-
-const stateReducer = (state, action) => {
-    console.log(state, action);
-    switch (action.type) {
-        case 'SET_VALUE':
-            const { index, value } = action.payload;
-            const newCoefficients = state.coefficients.map((ki, i) =>
-                i < index ? Math.max(ki, value) : i === index ? value : Math.min(ki, value),
-            );
-            return { ...state, coefficients: newCoefficients };
-
-        case 'ADD_COEFFICIENT':
-            return {
-                ...state,
-                coefficients: state.coefficients.concat([
-                    state.coefficients[state.coefficients.length - 1],
-                ]),
-            };
-        case 'REMOVE_COEFFICIENT':
-            return {
-                ...state,
-                coefficients: state.coefficients.slice(
-                    0,
-                    Math.max(1, state.coefficients.length - 1),
-                ),
-            };
-
-        default:
-            return state;
-    }
-};
-
 const initialState = {
-    coefficients: Array.from({ length: 5 }, (v, i) => 100),
-    // for final-form
     coefficient: Array.from({ length: 5 }, (v, i) => 100),
     dms: 9,
 };
@@ -132,73 +51,27 @@ const decorator = createDecorator({
     updates: (value, field, allValues) => {
         // console.log({ value, field, allValues });
         const index = parseFloat(field.substring('coefficient['.length, field.length - 1), 10);
-        const newCoefficients = allValues.coefficient.map((ki, i) =>
-            i < index ? Math.max(ki, value) : i === index ? value : Math.min(ki, value),
-        );
-        return { coefficient: newCoefficients };
+        const coefficient = allValues.coefficient;
+        if (
+            coefficient[index] >= coefficient[Math.max(0, index - 1)] ||
+            coefficient[index] <= coefficient[Math.min(coefficient.length - 1, index + 1)]
+        ) {
+            const newCoefficients = coefficient.map((ki, i) =>
+                i < index ? Math.max(ki, value) : i === index ? value : Math.min(ki, value),
+            );
+            return { coefficient: newCoefficients };
+        }
+        return { coefficient };
     },
 });
 
 const MixerConvolution = ({ onChange }) => {
     const classes = useStyles();
-    const [state, dispatch] = React.useReducer(stateReducer, initialState);
 
-    const { coefficients } = state;
-
-    console.log({ coefficients });
-    // React.useEffect(() => {
-    //     onChange(state);
-    // }, [onChange, state]);
-
-    const handleSliderChange = React.useCallback(
-        (event, newValue, index) =>
-            dispatch({ type: 'SET_VALUE', payload: { index, value: newValue } }),
-        [dispatch],
-    );
-
-    const handleAddCoefficient = React.useCallback(() => {
-        dispatch({ type: 'ADD_COEFFICIENT' });
-    }, []);
-
-    const handleDeleteCoefficient = React.useCallback(() => {
-        dispatch({ type: 'REMOVE_COEFFICIENT' });
-    }, []);
-
-    const handleSubmit = (values) => {};
+    const handleSubmit = (values) => { };
 
     return (
         <div>
-            <div className={classes.root}>
-                {coefficients.map((v, i) => (
-                    <VerticalSlider
-                        key={`coefficient-${i} `}
-                        index={i}
-                        value={v}
-                        min={0}
-                        max={100}
-                        step={1}
-                        onSliderChange={handleSliderChange}
-                    />
-                ))}
-            </div>
-            <div>
-                <IconButton
-                    aria-label="delete"
-                    color="primary"
-                    disabled={coefficients.length <= 1}
-                    onClick={() => dispatch({ type: 'REMOVE_COEFFICIENT' })}
-                >
-                    <RemoveCoefficient />
-                </IconButton>
-                <IconButton
-                    aria-label="add"
-                    color="primary"
-                    disabled={coefficients.length >= 21}
-                    onClick={() => dispatch({ type: 'ADD_COEFFICIENT' })}
-                >
-                    <AddCoefficient />
-                </IconButton>
-            </div>
             <div>
                 <Form
                     subscription={{}}
@@ -209,58 +82,47 @@ const MixerConvolution = ({ onChange }) => {
                     decorators={[decorator]}
                     mutators={{ ...arrayMutators }}
                     render={({ form }) => {
-                        const { values } = form.getState();
-
                         return (
                             <div className={classes.form}>
                                 <AutoSave save={handleSubmit} debounce={200} />
-                                <FieldArray name="customers">
-                                    {({ fields }) => (
-                                        <>
-                                            <div>
-                                                {fields.map((v, i) => (
-                                                    <Field
-                                                        name={`coefficient-${i}`}
-                                                        component={ProportionField}
-                                                        unit=""
-                                                        max="100"
-                                                        step={1}
-                                                    />
-                                                ))}
-                                            </div>
-                                            <div>
-                                                <IconButton
-                                                    aria-label="delete"
-                                                    color="primary"
-                                                    disabled={coefficients.length <= 1}
-                                                    onClick={() => fields.pop()}
-                                                >
-                                                    <RemoveCoefficient />
-                                                </IconButton>
-                                                <IconButton
-                                                    aria-label="add"
-                                                    color="primary"
-                                                    disabled={coefficients.length >= 21}
-                                                    onClick={() => fields.push(0)}
-                                                >
-                                                    <AddCoefficient />
-                                                </IconButton>
-                                            </div>
-                                        </>
-                                    )}
+                                <FieldArray name="coefficient">
+                                    {({ fields }) => {
+                                        // console.log({ fields });
+                                        return (
+                                            <>
+                                                <div className={classes.verticalSlider}>
+                                                    {fields.map((v, i) => (
+                                                        <Field
+                                                            name={`coefficient[${i}]`}
+                                                            component={VerticalProportionField}
+                                                            unit=""
+                                                            valueLabelDisplay="auto"
+                                                            orientation="vertical"
+                                                        />
+                                                    ))}
+                                                </div>
+                                                <div>
+                                                    <IconButton
+                                                        aria-label="delete"
+                                                        color="primary"
+                                                        disabled={fields.length <= 1}
+                                                        onClick={() => fields.pop()}
+                                                    >
+                                                        <RemoveCoefficient />
+                                                    </IconButton>
+                                                    <IconButton
+                                                        aria-label="add"
+                                                        color="primary"
+                                                        disabled={fields.length >= 21}
+                                                        onClick={() => fields.push(0)}
+                                                    >
+                                                        <AddCoefficient />
+                                                    </IconButton>
+                                                </div>
+                                            </>
+                                        );
+                                    }}
                                 </FieldArray>
-
-                                {/* <div className={classes.root}>
-                                    {values.coefficient.map((v, i) => (
-                                        <Field
-                                            name={`coefficient[${i}]`}
-                                            component={ProportionField}
-                                            unit=""
-                                            max="100"
-                                            step={1}
-                                        />
-                                    ))}
-                                </div> */}
 
                                 <Field
                                     name="dms"
